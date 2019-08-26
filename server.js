c29eced1698a55b32868babd17e5c19da56adeab
kpan0006 <kpan0006@student.monash.edu>
2019-08-26 21:35:03 +1000

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+let express = require('express');
+let app = express();
+
+//set up the view engine
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+let bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended:false}));
+
+var db =[];
+
+
+
+app.use(express.static('img'));
+
+var filePath = __dirname + "/views/";
+
+app.get("/",function(req,res){
+    let fileName = filePath + "index.html";
+    res.sendfile(fileName);
+});
+
+app.get("/newtask",function(req,res){
+    let fileName = filePath + "newTask.html";
+    res.sendFile(fileName);
+});
+
+app.get("/listtasks",function(req,res){
+    res.render("listTasks",{mycustomers:db});
+});
+
+app.post("/listtasks",function(req,res){
+    db.push(req.body);
+    //first: HTML filename    second : my customers:propertyName, value is db, can be several properties
+    //render before sending
+    res.render("listTasks",{mycustomers:db});
+
+});
+
+
+
+app.listen('8088',()=>{
+    console.log('server started... ...');
+})
